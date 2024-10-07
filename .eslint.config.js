c00206315de0693b24cece3096513de58cdad22d
GonzaSab <gonza.sabaino@gmail.com>
2024-10-07 16:46:51 +0100

    Eslint upgrade

---
+++ .eslint.config.js
@@ -0,0 +1,20 @@
+export default [
+    {
+        files: ["**/*.js"], // Archivos a los que se aplicarán las reglas
+        env: {
+            browser: true,
+            commonjs: true,
+            es2021: true,
+            node: true, // Se añade directamente el entorno de Node.js
+        },
+        parserOptions: {
+            ecmaVersion: 'latest',
+            sourceType: 'module', // Se ajusta si estás usando módulos ES (import/export)
+        },
+        plugins: ['bot-whatsapp'],
+        extends: ['plugin:bot-whatsapp/recommended'],
+        rules: {
+            // Puedes agregar reglas personalizadas aquí si es necesario
+        },
+    },
+];
